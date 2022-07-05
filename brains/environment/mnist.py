7cdb26c9ff63f95d44491c8272026eceb57f93e1
Peter Lonjers <utilitarianexe@gmail.com>
2022-07-04 22:43:28 -0700

    Add type annotations. Starting with one fire to experiment.

--- brains/environment/mnist.py
+++ brains/environment/mnist.py
@@ -1,5 +1,6 @@
 import brains.utils as utils
 import brains.environment.base as base
+from typing import Optional
 
 from collections import defaultdict
 import random
@@ -7,7 +8,9 @@ import random
 # copy paste of handwriting
 # magic numbers
 class MnistEnvironment(base.BaseEpochChallengeEnvironment):
-    def __init__(self, epoch_length, input_delay=0, shuffle=True, number_of_possible_outputs=10):
+    def __init__(self, epoch_length: int,
+                 input_delay: int = 0, shuffle: bool = True,
+                 number_of_possible_outputs: int = 10):
         super().__init__(epoch_length, input_delay)
         self._possible_outputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         self._possible_outputs = self._possible_outputs[:number_of_possible_outputs]
@@ -22,7 +25,7 @@ class MnistEnvironment(base.BaseEpochChallengeEnvironment):
         if shuffle:
             random.shuffle(self._labels_and_images)
 
-    def stimuli(self, step):
+    def stimuli(self, step: int) -> set:
         real_step = step - self._input_delay
         is_correct_time = real_step % self._epoch_length == 0 and real_step >= 0
         if not is_correct_time:
@@ -42,7 +45,7 @@ class MnistEnvironment(base.BaseEpochChallengeEnvironment):
                 stimuli.add((x, y, 0.3,))
         return stimuli
 
-    def desired_output_id(self, step):
+    def desired_output_id(self, step: int) -> Optional[int]:
         real_step = step - self._input_delay
         if real_step//self._epoch_length >= len(self._labels_and_images):
             return None
@@ -54,7 +57,7 @@ class MnistEnvironment(base.BaseEpochChallengeEnvironment):
         (label, image) = self._labels_and_images[image_index]
         return label
 
-def negate(image):
+def negate(image: list) -> list:
     new_image = []
     for pixel in image:
         new_pixel = abs(pixel - 255)
@@ -64,7 +67,8 @@ def negate(image):
             new_image.append(0.0)
     return new_image
 
-def read(image_file_name, label_file_name, number_of_images_to_read, possible_outputs):
+def read(image_file_name: str, label_file_name: str,
+         number_of_images_to_read: int, possible_outputs: list) -> dict:
     label_file = open(utils.data_dir_file_path(label_file_name), "rb")
     image_file = open(utils.data_dir_file_path(image_file_name), "rb")
 
@@ -72,7 +76,7 @@ def read(image_file_name, label_file_name, number_of_images_to_read, possible_ou
     image_file.read(16)   
     label_file.read(8)
 
-    images_by_label = defaultdict(list)
+    images_by_label: dict[Optional[int], list] = defaultdict(list)
     for _ in range(number_of_images_to_read):
         label = ord(label_file.read(1))
         image = []
@@ -91,7 +95,7 @@ def read(image_file_name, label_file_name, number_of_images_to_read, possible_ou
     label_file.close()
     return images_by_label
 
-def read_mnist(possible_outputs):
+def read_mnist(possible_outputs: list):
     return read("train-images.idx3-ubyte", "train-labels.idx1-ubyte", 10000, possible_outputs)
 
 if __name__ == '__main__':
